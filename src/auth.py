1b5b4017b0fcdba9e44713dae28f7c6b9dfa218c
tunaDari <tunahan.darii@gmail.com>
2022-01-06 16:57:53 +0300

    Add refreshing token

--- src/auth.py
+++ src/auth.py
@@ -83,5 +83,13 @@ def me():
         'email': user.email,
     }), HTTP_200_OK
 
-    return {"user": "me"}
+@auth.post('/token/refresh')
+@jwt_required(refresh=True)
+def refresh_users_token():
+    identity = get_jwt_identity()
+    access = create_access_token(identity=identity)
+
+    return jsonify({
+        'access': access
+    }), HTTP_200_OK
 
